be0e8fb0bf3a6211c094ba21527929c1a983cf9d
Suheb <suhebjamadar@hotmail.com>
2017-09-17 09:28:52 +0530

    Segregated carousel template

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
-  templateUrl: './home.html'
+    templateUrl: './home.html'
 })
 
 export class HomeComponent  {  }
--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -9,10 +9,11 @@ import { LayoutComponent } from '../components/layout/layout.component';
 import { HomeComponent } from '../components/home/home.component';
 import { AboutComponent } from '../components/about/about.component';
 import { ContactComponent } from '../components/contact/contact.component';
+import { CarouselComponent } from '../components/carousel/carousel.component';
 
 @NgModule({
   imports:      [ BrowserModule, Routing ],
-  declarations: [ AppComponent, LayoutComponent, HomeComponent , AboutComponent, ContactComponent ],
+  declarations: [ AppComponent, LayoutComponent, HomeComponent, AboutComponent, ContactComponent, CarouselComponent ],
   providers:    [ {provide: APP_BASE_HREF, useValue: '/'}] ,
   bootstrap:    [ AppComponent ]
 })
